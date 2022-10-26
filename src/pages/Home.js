70fddfb8bb0c45a5c9552abf1736a3bf36e27b3f
Rezaul <mdrezaulkarrim@gmail.com>
2022-10-26 14:56:42 +0600

    added home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,38 @@
+import { Link } from "react-router-dom";
+import img from "../img/arnold-francisca-f77Bh3inUpE-unsplash.jpg";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="justify-between gap-10 p-5 space-y-10 bg-white rounded-md lg:flex lg:space-y-0">
+      <div className="flex-1">
+        <h1 className="text-2xl font-semibold text-black sm:text-3xl md:text-4xl">
+          Learn anywhere. From any distance
+          <span className="font-bold text-blue-500">...</span>
+        </h1>
+        <p className="mt-6 text-base font-semibold text-gray-700 sm:text-lg sm:mt-9 lg:mt-12 ">
+          Solo learn community provide you their best to accomplish your desire.
+          Our interactive lesson will boost your knowledge power to do something
+          better in our carrieer.
+        </p>
+        <div className="flex gap-6 mt-10 text-base font-semibold sm:text-lg lg:mt-20">
+          <Link
+            to="/courses"
+            className="flex items-center px-2 py-1 text-white duration-300 bg-blue-500 rounded-md shadow-sm cursor-pointer sm:px-4 sm:py-2 hover:bg-blue-600 active:bg-blue-700 transorm transtion active:translate-y-1 focus:ring-2 focus:ring-offset-4 focus:ring-blue-600 shadow-blue-300 active:shadow-lg backface-hidden"
+          >
+            Enroll Now
+          </Link>
+          <button className="px-2 py-1 border-4 border-blue-500 rounded-md sm:px-4 sm:py-2 ">
+            Read More
+          </button>
+        </div>
+      </div>
+
+      <div className="w-full lg:w-1/2">
+        <img
+          className="rounded-md h-96 sm:h-[450px] w-full object-cover object-center shadow-md shadow-blue-100"
+          src={img}
+          alt="Solo Learn"
+        />
+      </div>
+    </div>
+  );
 }
--- src/pages/shared/Header.js
+++ src/pages/shared/Header.js
@@ -29,17 +29,17 @@ export default function Header() {
   return (
     <div className="items-center justify-between px-3 py-3 mt-5 space-y-6 bg-blue-500 rounded shadow md:px-4 md:space-y-0 md:flex shadow-blue-400">
       <Link to="/">
-        <h1 className="text-xl font-semibold text-white sm:text-2xl">
+        <h1 className="px-2 md:px-0 text-xl font-semibold text-white sm:text-2xl">
           Solo Learn
         </h1>
       </Link>
-      <nav className="ml-auto mr-10 space-x-4 md:space-x-3 text-gray-50 xs:ml-0 xs:space-x-0 xs:mr-0 xs:flex xs:flex-col xs:w-full">
+      <nav className="ml-auto mr-7 space-x-4 md:space-x-3 text-gray-50 xs:ml-0 xs:space-x-0 xs:mr-0 xs:flex xs:flex-col xs:w-full">
         {navItems.map((el, i) => (
           <NavLink
             className={({ isActive }) =>
               isActive
-                ? "text-base sm:text-lg lg:text-lg bg-white text-blue-500 px-2 py-[6px] rounded font-semibold text-center transition duration-300   w-20 xs:w-full"
-                : "text-base sm:text-lg lg:text-lg px-2 py-[6px] rounded font-semibold text-center w-20 transition duration-300 xs:w-full  hover:text-blue-200"
+                ? "text-base sm:text-lg lg:text-lg bg-white text-blue-500 px-2 py-[6px] rounded font-semibold text-center transition duration-300 w-20 inline-block "
+                : "text-base text-white sm:text-lg lg:text-lg px-2 py-[6px] rounded font-semibold text-center w-20 inline-block transition duration-300   hover:text-blue-200"
             }
             key={i}
             to={el.path}
@@ -49,45 +49,52 @@ export default function Header() {
         ))}
       </nav>
 
-      {currentUser?.uid ? (
-        <Link to="/profile">
-          <div className="flex items-center gap-3 ">
-            <img
-              className="w-10 h-10 rounded-full"
-              src={currentUser?.photoURL}
-              alt="user"
-            />
-            <span className="font-medium text-white transition duration-300 hover:text-blue-200 ">
-              {currentUser?.displayName}
-            </span>
+      <div className="flex gap-2 xs:gap-4 items-center">
+        {currentUser?.uid ? (
+          <div className="mt-2 px-2 md:px-0  sm:mt-0">
+            <Link to="/profile">
+              <div className="flex items-center gap-3 ">
+                {currentUser?.photoURL ? (
+                  <img
+                    className="w-10 h-10 rounded-full"
+                    src={currentUser.photoURL}
+                    alt="user"
+                  />
+                ) : (
+                  ""
+                )}
+                <span className="font-medium text-white transition duration-300 hover:text-blue-200 ">
+                  {currentUser?.displayName}
+                </span>
+              </div>
+            </Link>
           </div>
-        </Link>
-      ) : (
-        <div className="flex items-center gap-3">
+        ) : (
           <NavLink
             className={({ isActive }) =>
               isActive
-                ? "text-base sm:text-lg lg:text-lg bg-white text-blue-500 px-2 py-[6px] rounded font-semibold text-center transition duration-300   w-20 xs:w-full"
-                : "text-base text-white sm:text-lg lg:text-lg px-2 py-[6px] rounded font-semibold text-center w-20 transition duration-300 xs:w-full  hover:text-blue-200"
+                ? "text-base sm:text-lg lg:text-lg bg-white text-blue-500 px-2 py-[6px] rounded font-semibold text-center transition duration-300 w-20 "
+                : "text-base text-white sm:text-lg lg:text-lg px-2 py-[6px] rounded font-semibold text-center w-20 transition duration-300   hover:text-blue-200"
             }
             to="/login"
           >
             Login
           </NavLink>
-          <Switch
-            onChange={handleSwitch}
-            onHandleColor="#fff"
-            offHandleColor="#3b82f6"
-            checked={toggle}
-            offColor="#fff"
-            onColor="#60a5fa"
-            uncheckedIcon={<HiOutlineSun className="w-full h-full" />}
-            checkedIcon={
-              <HiOutlineMoon className="w-full h-full text-white font-semibold" />
-            }
-          />
-        </div>
-      )}
+        )}
+
+        <Switch
+          onChange={handleSwitch}
+          onHandleColor="#fff"
+          offHandleColor="#3b82f6"
+          checked={toggle}
+          offColor="#fff"
+          onColor="#60a5fa"
+          checkedIcon={<HiOutlineSun className="w-full h-full text-white" />}
+          uncheckedIcon={
+            <HiOutlineMoon className="w-full h-full text-blue-400" />
+          }
+        />
+      </div>
     </div>
   );
 }
